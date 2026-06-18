21074280dd4e54c701f789fe6ed3510c4edd918c
MuhArrafifPrikusuma <muhammadarrafifa1308@gmail.com>
2026-06-18 11:32:25 +0800

    recall

--- leetCodeAnswer.c
+++ leetCodeAnswer.c
@@ -130,57 +130,9 @@ char *longestCommonPrefix(char **strs, int strsSize) {
  * leetCode #15
  */
 // FIXME: able to return in 2D array but only able to take the very first array
-int compareAscend(const void *a, const void *b) {
-  return *(int *)a - *(int *)b;
-}
+
 int **threeSum(int *nums, int numsSize, int *returnSize,
-               int **returnColumnSizes) {
-  qsort(nums, numsSize, sizeof(int), compareAscend);
-
-  int capacity = 16;
-  int **result = malloc(capacity * sizeof(int));
-  *returnSize = 0;
-
-  for (int i = 0; i < numsSize - 2; i++) {
-    if (nums[i] > 0)
-      break;
-    if (i > 0 && nums[i] == nums[i - 1])
-      continue;
-
-    int left = i + 1;
-    int right = numsSize - 1;
-    if (left < right) {
-      int sum = nums[i] + nums[left] + nums[right];
-      if (sum == 0) {
-        if (*returnSize >= capacity) {
-          capacity *= 2;
-          result = realloc(result, capacity * sizeof(int *));
-        }
-        result[*returnSize] = malloc(3 * sizeof(int));
-        result[*returnSize][0] = nums[i];
-        result[*returnSize][1] = nums[left];
-        result[*returnSize][2] = nums[right];
-        // () to prevent unused expression
-        (*returnSize)++;
-        while (left < right && nums[left] == nums[left + 1])
-          left++;
-        while (left < right && nums[right] == nums[right - 1])
-          right--;
-        left++;
-        right--;
-      } else if (sum < 0) {
-        left++;
-      } else {
-        right++;
-      }
-    }
-  }
-  *returnColumnSizes = malloc(*returnSize * sizeof(int));
-  for (int i = 0; i < *returnSize; i++) {
-    (*returnColumnSizes)[i] = 3;
-  }
-  return result;
-}
+               int **returnColumnSizes) {}
 
 // # 2 add Two Numbers
 struct ListNode {
@@ -439,3 +391,9 @@ char *intToRoman(int num) {
   }
   return result;
 }
+// TODO: finish this later
+//  #17. Letter Combinations of a Phone number
+
+static char *phoneMaps[] = {"",    "",    "abc",  "def", "ghi",
+                            "jkl", "mno", "pqrs", "tuv", "wxyz"};
+char **letterCombinations(char *digits, int *returnSize) {}
