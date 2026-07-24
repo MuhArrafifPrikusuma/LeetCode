b3ff0b89b09f781e06848b86d0f12c22e8f5994f
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-07-24 11:39:04 +0800

    3sum

--- leetCodeAnswer.c
+++ leetCodeAnswer.c
@@ -126,13 +126,6 @@ char *longestCommonPrefix(char **strs, int strsSize) {
   }
   return base;
 }
-/*
- * leetCode #15
- */
-// FIXME: able to return in 2D array but only able to take the very first array
-
-int **threeSum(int *nums, int numsSize, int *returnSize,
-               int **returnColumnSizes) {}
 
 // # 2 add Two Numbers
 struct ListNode {
@@ -391,9 +384,59 @@ char *intToRoman(int num) {
   }
   return result;
 }
-// TODO: finish this later
-//  #17. Letter Combinations of a Phone number
 
-static char *phoneMaps[] = {"",    "",    "abc",  "def", "ghi",
-                            "jkl", "mno", "pqrs", "tuv", "wxyz"};
-char **letterCombinations(char *digits, int *returnSize) {}
+// #15. 3sum
+int compare(const void *a, const void *b) { return (*(int *)a - *(int *)b); }
+int **threeSum(int *nums, int numsSize, int *returnSize,
+               int **returnColumnSizes) {
+  qsort(nums, numsSize, sizeof(int), compare);
+
+  int capacity = 1000;
+  int **result = (int **)malloc(capacity * sizeof(int *));
+  *returnColumnSizes = (int *)malloc(capacity * sizeof(int));
+  *returnSize = 0;
+
+  for (int i = 0; i < numsSize - 2; i++) {
+    if (nums[i] > 0)
+      break;
+
+    if (i > 0 && nums[i] == nums[i - 1])
+      continue;
+
+    int left = i + 1;
+    int right = numsSize - 1;
+
+    while (left < right) {
+      int sum = nums[i] + nums[left] + nums[right];
+
+      if (sum == 0) {
+        if (*returnSize >= capacity) {
+          capacity *= 2;
+          result = (int **)realloc(result, capacity * sizeof(int *));
+          *returnColumnSizes =
+              (int *)realloc(*returnColumnSizes, capacity * sizeof(int));
+        }
+
+        result[*returnSize] = (int *)malloc(3 * sizeof(int));
+        result[*returnSize][0] = nums[i];
+        result[*returnSize][1] = nums[left];
+        result[*returnSize][2] = nums[right];
+        (*returnColumnSizes)[*returnSize] = 3;
+        (*returnSize)++;
+
+        while (left < right && nums[left] == nums[left + 1])
+          left++;
+        while (left < right && nums[right] == nums[right - 1])
+          right--;
+
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return result;
+}
